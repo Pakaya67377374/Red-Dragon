0ea01b5c53d277d2f9006394176562d0ca694d8f
Pakaya67377374 <109017526+Pakaya67377374@users.noreply.github.com>
2022-07-10 19:24:04 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -26,7 +26,7 @@ global.owner = ['+380944814219'] //Owner number in aive msg
 global.premium = ['+380944814219'] //Owner Number info
 global.ownernomer = '+380944814219' //Owner Number <<<
 
-global.ownername = '😎𝙆.𝙋𝙧𝙖𝙗𝙝𝙖𝙨𝙝𝙖' //Owner Name
+global.ownername = '🎭ＡＣＨＩＮＴＨＡ🎭' //Owner Name
 global.botname = ' ☬RED༒DRAGON࿐' //Bot Name
 
 global.button = '🤭Youtube📍' //Costomize A Button Name In Alive Message
